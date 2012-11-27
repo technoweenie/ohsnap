fb2a6d9dc6ad7e408380f50e04a51053a9247f04
rick <technoweenie@gmail.com>
2012-11-27 15:34:25 -0700

    awwwww snap yo

--- lib/ohsnap.rb
+++ lib/ohsnap.rb
@@ -1,6 +1,10 @@
 require 'ohsnap/ohsnap'
 require 'ohsnap/version'
 
+suffixes = {'a' => 'w', 'o' => 'h'}
+prefix = suffixes.keys[rand(suffixes.size)]
+warn "#{prefix}#{suffixes[prefix] * (rand(10)+1)} snap!"
+
 module Ohsnap
   extend self
 end
